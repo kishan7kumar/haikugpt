501bd267f4a5bbbcc685bc1afe68a77bba8ce744
kishan7kumar <kishankumar3202@gmail.com>
2024-01-22 09:42:10 -0800

    Add sqlite connection and update config file for db path

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 import createHttpError from "http-errors";
 import morgan from "morgan";
 import cors from "cors";
-import { PORT } from "./config";
+import { Database } from "sqlite3";
+import { PORT, DB_PATH } from "./config";
 import { errorHandler } from "./middleware/errorHandler";
+import * as fs from "fs";
 
-
+// Setting up global SQLite DB instance so that we don't have to create a new connection for every request
+// tslint:disable-next-line
+global.SQLiteDBInstance = null;
 
 const app = express();
 app.use(express.json());
@@ -21,6 +25,31 @@ app.use(
 app.use(() => {
   throw createHttpError(404, "Not found");
 });
+
+// Function to create a connection to the SQLite DB
+function createDatabaseConnection() {
+  if (fs.existsSync(DB_PATH)) {
+    global.SQLiteDBInstance = new Database(DB_PATH);
+  } else {
+    const db: Database = new Database(DB_PATH, (error: any) => {
+      if (error) {
+        return console.error(error.message);
+      }
+    });
+    db.exec(`
+            CREATE TABLE haikuqueries
+            (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              userquery TEXT NOT NULL
+            );
+    `);
+    console.log("SQLite DB created");
+    global.SQLiteDBInstance = db;
+  }
+  console.log("Database connnection successful");
+}
+
+createDatabaseConnection();
 
 app.set("port", PORT);
 
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,3 +2,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const DB = process.env.DB!;
 export const PORT = process.env.PORT;
+export const DB_PATH = process.env.DB_PATH!;
